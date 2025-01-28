4586da936291d9bbbe383b8a367be92616f2dc3b
SYEDA ESHA <syedaesha405@gmail.com>
2025-01-28 12:24:49 +0500

    second version

--- java.js
+++ java.js
@@ -9,7 +9,7 @@ blr.style.left=dets.x - 250+"px"
 blr.style.top=dets.y -250+"px"
 });
 gsap.to("#navi",{
-    backgroundColor:"#5f5dc9",
+    backgroundColor:"green",
     duration:0.5,
     height:"90px",
   
@@ -18,4 +18,63 @@ gsap.to("#navi",{
         start:"top -10%",end:"top -11%",scrub:1
     }
    
+});
+gsap.to(".cards", {
+  y: 30,
+  opacity: 1,
+  duration: 1,
+  stagger: 0.1,
+  scrollTrigger: {
+    trigger: ".cards-container", 
+    scroller: "body", 
+    // markers: true, 
+    start: "top 60%", 
+    end: "top 90%", 
+    scrub: 2, 
+  },
+});
+gsap.to(".doctor-cards", {
+  x: 30,
+  opacity: 1,
+  duration: 1,
+  stagger: 1,
+  scrollTrigger: {
+    trigger: ".doctors-section", 
+    scroller: "body", 
+    // markers: true, 
+    start: "top 60%", 
+    end: "top 10%", 
+    scrub: 3, 
+  },
+});
+gsap.from("#h11",{
+  scale:1.2,
+  duration: 1,
+  scrub: 3,
+  scrollTrigger: {
+    trigger: ".container", 
+    scroller: "body",
+    markers: true, 
+    start: "top 60%", 
+    end: "top 80%", 
+    scrub: 3, 
+  },
+})
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
 });
